96932e22e631dbf5d06a30341e0eb4dc3f9f4468
FaithAdline <adlinefaith@gmail.com>
2023-10-02 17:53:32 +0300

    assignment completed

---
+++ index.js
@@ -0,0 +1,37 @@
+lapsedUserAccounts.map( u => sendBillTo(u.address) );
+function divide(a, b) {
+  return a / b;
+  test('divide divides 2000 by 100', () => {
+    expect(divide(2000, 100)).toBe(20);
+  });
+}
+let division = divide(2000, 100); 
+const square = (parameter) => {
+  return parameter * parameter;
+};
+
+const add = (parameter1, parameter2) => {
+  return parameter1 + parameter2;
+};
+
+describe("Test function add",() =>{
+  expect(add(parameter1+parameter2));
+});
+test('add arrow function takes two parameters and adds them together', () => {
+  expect(add(10, 5)).toBe(15);
+});
+
+
+
+  
+  
+  
+
+
+  
+  
+  
+
+  
+  
+  
